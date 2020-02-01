1c7737e0b1aa1786bfdc29dae93c20657f63d182
Mohamed <mohamed.hesh.asad@gmail.com>
2020-02-02 01:17:26 +0200

    Dilvery with worked clock for GPIO

--- tm4c123_firmware/Debug/appl/lab_00_nvic.d
+++ tm4c123_firmware/Debug/appl/lab_00_nvic.d
@@ -2,6 +2,10 @@
 
 appl/lab_00_nvic.obj: D:/Folders/tm4c123_firmware/appl/lab_00_nvic.c
 appl/lab_00_nvic.obj: D:/Folders/tm4c123_firmware/appl/../mcal/nvic/nvic.h
+appl/lab_00_nvic.obj: D:/Folders/tm4c123_firmware/appl/../mcal/SysCtr/SysCtr.h
+appl/lab_00_nvic.obj: D:/Folders/tm4c123_firmware/appl/../mcal/SysCtr/../../utils/Std_Types.h
 
 D:/Folders/tm4c123_firmware/appl/lab_00_nvic.c: 
 D:/Folders/tm4c123_firmware/appl/../mcal/nvic/nvic.h: 
+D:/Folders/tm4c123_firmware/appl/../mcal/SysCtr/SysCtr.h: 
+D:/Folders/tm4c123_firmware/appl/../mcal/SysCtr/../../utils/Std_Types.h: 
